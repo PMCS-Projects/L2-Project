89c2785f7e1b382af5413c789ff1bca734b10d12
CodingEmpire <codykneale@yahoo.com>
2023-12-08 16:19:47 -0800

    Changes Background Color
    Made the background color less harsh on the eyes for those that want to take a quick look at it. It was a flashbang before but now it is in nightmode.

--- Main.py
+++ Main.py
@@ -4,12 +4,14 @@ import sys
 import math
 
 # Variables to create colors
-white = (255, 255, 255)
+bg_Color = pygame.Color("#18191A")
 yellow = (255, 255, 0)
 blue = (0, 0, 255)
 
+x = 600
+y = 400
 # Screen size
-screen = pygame.display.set_mode((1000, 1000))
+screen = pygame.display.set_mode((x, y), pygame.RESIZABLE)
 
 # Name of the window
 pygame.display.set_caption('Sun and Earth Simulation')
@@ -18,8 +20,10 @@ pygame.display.set_caption('Sun and Earth Simulation')
 pygame.init()
 
 # Set up sun properties
+sunX = x * .5
+sunY = y * .5
 sun_radius = 50
-sun_position = (0, 0)
+sun_position = (sunX, sunY)
 
 # Set up earth properties
 earth_radius = 20
@@ -38,10 +42,14 @@ while running:
             running = False
 
     # Clear the screen
-    screen.fill(white)
+    screen.fill(bg_Color)
 
     # Draw the sun
     pygame.draw.circle(screen, yellow, sun_position, sun_radius)
+    # position the sun
+    sunX = x * .5
+    sunY = y * .5
+    sun_position = (sunX, sunY)
 
     # Update earth position
     earth_x = sun_position[0] + earth_distance * math.cos(math.radians(earth_angle))
@@ -58,6 +66,9 @@ while running:
 
     # Cap the frame rate
     clock.tick(fps)
+
+    # screen size
+    x, y = screen.get_size()
 
 # Quit pygame and exit the program
 pygame.quit()
